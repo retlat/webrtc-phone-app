4c4d8cf411848fc525fa06368f8d66715faff241
retlat <16059710+retlat@users.noreply.github.com>
2022-05-01 16:36:38 +0900

    resource の module を作成

---
+++ android/resource/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 30
+        targetSdk = 31
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+}
